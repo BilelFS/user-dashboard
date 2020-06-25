9326960f48435ffcde1236f9a931de60e39d5fcd
BilelFS <fs.bilel@gmail.com>
2020-06-25 13:50:21 +0200

    fix the port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081",
+  origin: "http://localhost:8080",
 };
 
 app.use(cors(corsOptions));
